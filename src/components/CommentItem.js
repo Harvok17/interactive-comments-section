7745b24f01da9df61343a6ca9279e27bc2c5bba1
Harvey Granfil <jhgranfil@gmail.com>
2022-05-25 14:01:39 +0800

    added elements on CommentItem component.

--- src/components/CommentItem.js
+++ src/components/CommentItem.js
@@ -4,22 +4,75 @@ import "./VoteButton.js";
 export class CommentItem extends LitElement {
   static properties = {
     score: { type: Number },
-    vote: { type: String, reflect: true },
   };
 
   static styles = css`
-    :host {
+    /* :host {
       display: flex;
       height: 120px;
       min-height: 120px;
       border-radius: 10px;
       background-color: var(--very-light-gray);
       padding: 20px;
+    }  */
+
+    p {
+      line-height: 1.5em;
     }
 
-    .wrapper {
-      height: 100%;
-      width: 100%;
+    .comment,
+    .reply {
+      min-height: 120px;
+      display: flex;
+      border-radius: 10px;
+      background-color: var(--very-light-gray);
+      padding: 20px;
+    }
+
+    .reply {
+      margin-top: 10px;
+      align-items: flex-start;
+    }
+
+    .reply textarea {
+      flex: 1;
+      margin-inline: 20px;
+      height: 60px;
+      border-radius: 10px;
+      resize: none;
+      padding: 20px;
+      font-family: inherit;
+    }
+
+    .reply img {
+      height: 50px;
+      width: 50px;
+    }
+
+    .comment__details {
+      flex: 1;
+      padding-left: 20px;
+    }
+
+    .comment__header {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .avatar {
+      display: flex;
+      align-items: center;
+    }
+
+    .avatar img {
+      height: 30px;
+      width: 30px;
+      margin-right: 20px;
+    }
+
+    .avatar__username {
+      font-weight: bold;
+      margin-right: 20px;
     }
   `;
 
@@ -29,37 +82,42 @@ export class CommentItem extends LitElement {
   }
 
   updateScore(e) {
-    this.vote = e.detail.value;
-  }
-
-  willUpdate(changedProps) {
-    if (changedProps.has("vote")) {
-      const newValue = this.vote;
-      const oldValue = changedProps.get("vote");
-
-      if (newValue === "up") {
-        if (oldValue === "down") {
-          this.score += 2;
-        } else {
-          this.score += 1;
-        }
-      } else if (newValue === "down") {
-        if (oldValue === "up") {
-          this.score -= 2;
-        } else {
-          this.score -= 1;
-        }
-      }
-    }
+    console.log(e.target.score);
   }
 
   render() {
-    return html`<div class="wrapper">
-      <vote-button
-        @onVote=${this.updateScore}
-        .score=${this.score}
-      ></vote-button>
-    </div>`;
+    return html`<div class="comment">
+        <vote-button
+          @vote-changed=${this.updateScore}
+          .score=${this.score}
+        ></vote-button>
+        <div class="comment__details">
+          <div class="comment__header">
+            <div class="avatar">
+              <img
+                src="./images/avatars/image-amyrobson.png"
+                alt="user image"
+              />
+              <span class="avatar__username">amyrobson</span>
+              <span class="avatar__comment-date">1 month ago</span>
+            </div>
+            <button>Reply</button>
+          </div>
+          <p>
+            Impressive! Though it seems the drag feature could be improved. But
+            overall it looks incredible. You've nailed the design and the
+            responsiveness at various breakpoints works really well.
+          </p>
+        </div>
+      </div>
+      <div class="reply">
+        <img
+          src="./images/avatars/image-juliusomo.png"
+          alt="current user image"
+        />
+        <textarea></textarea>
+        <button>Reply</button>
+      </div> `;
   }
 }
 
